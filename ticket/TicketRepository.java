8d304408d2ab4bfd539d886c002161d370721857
m40r1 <99940252+m40r1@users.noreply.github.com>
2022-10-23 11:51:39 -0300

    Delete ticket directory

--- ticket/TicketRepository.java
+++
@@ -1,9 +0,0 @@
-package backend.json.teste.ticket;
-
-import java.util.UUID;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TicketRepository extends JpaRepository<Ticket, UUID> {
-	// TODO FindByidDestino
-}
