a90e8adc190ea5432dd784e5c0ac3fecf5d8661e
m40r1 <99940252+m40r1@users.noreply.github.com>
2022-11-20 21:44:59 +0000

    Configuracoes de seguracanca para spa
    pretendo mudar para o spring gateway

--- src/main/java/backend/json/teste/sec/AppSecConf.java
+++ src/main/java/backend/json/teste/sec/AppSecConf.java
@@ -1,89 +1,60 @@
 package backend.json.teste.sec;
 
-import org.springframework.beans.factory.annotation.Autowired;
+// import java.net.http.HttpHeaders;
+// import java.net.http.HttpRequest;
+// import java.util.UUID;
+// import java.util.concurrent.TimeUnit;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-import java.net.http.HttpHeaders;
-import java.net.http.HttpRequest;
-import java.util.UUID;
-import java.util.concurrent.TimeUnit;
-
-import static backend.json.teste.sec.AppRoles.*;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
-@EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class AppSecConf extends WebSecurityConfigurerAdapter {
+// FIXME refazer com filter_chain
+public class AppSecConf {
+@Bean
+  SecurityFilterChain MainSec(HttpSecurity http) throws Exception {
+  return http
+          //TODO integrar com react?
+          xs.csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+          .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+          .formLogin(form -> form
+                  .successHandler((request, response, authentication) -> response.setStatus(HttpStatus.OK.value()))
+                  .failureHandler((request, response, exception) -> response.setStatus(HttpStatus.UNAUTHORIZED.value()))
+                  )
+          .exceptionHandling(ex -> ex.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+          .build();
+}
 
-    private final PasswordEncoder passwordEncoder;
-
-
-    @Autowired
-    public AppSecConf(PasswordEncoder passwordEncoder) {
-        this.passwordEncoder = passwordEncoder;
-    }
-
-    // TODO usar apenas para teste local
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()//TODO usando para facilitar o teste das apis
-                .authorizeRequests()
-                /*.antMatchers("/destino**", "/ticket**", "/perfil**")
-                .hasRole(ADMIN.name())
-                .antMatchers("/ticket/*", "/destino**")
-                .hasRole(AGENTE.name())
-                .antMatchers("/perfil**", "/destino**")
-                .hasRole(PERFIL.name())*/
-                //TODO limitacao nos metodos em cada url
-                .antMatchers("/perfil/cadastro", "/perfil/login", "/erro", "/destino", "/destino/*")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login").permitAll()
-                .defaultSuccessUrl("/destino", true)
-                .and()
-                .rememberMe()
-                .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
-                .key(UUID.randomUUID().toString());
-
-    }
+@Bean
+  UserDetailsService userDetailsService() {
+  User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+}
 
 
-    //TODO jamais colocar senha no codigo caso ele for publico
-    @Override
-    @Bean
-    protected UserDetailsService userDetailsService() {
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder.encode("senha"))
-                .roles(ADMIN.name())
-                .build();
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+    config.addAllowedHeader("*");
+    config.addAllowedMethod("*");
+//endereco servidor react
+    config.addAllowedOrigin("http://localhost:4200");
+ config.setAllowCredentials(true);
+ source.registerCorsConfiguration("/**",config);
+ return  source;
+  }
 
-        UserDetails agente = User.builder()
-                .username("agente")
-                .password(passwordEncoder.encode("senha"))
-                .roles(AGENTE.name())
-                .build();
-
-        UserDetails cliente = User.builder()
-                .username("cliente")
-                .password(passwordEncoder.encode("senha"))
-                .roles(PERFIL.name())
-                .build();
-
-        return new InMemoryUserDetailsManager(admin, agente, cliente);
-    }
 }
